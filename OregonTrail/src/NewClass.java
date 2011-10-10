a3f7dff509a17256813fc08edd117e2878af4a97
Ira <iradkaplan@gmail.com>
2011-10-10 15:58:29 -0400

    added a new class to play around with

---
+++ OregonTrail/src/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author Ira
+ */
+public class NewClass {
+    private void testMethod(){
+        int i = 37;
+    }
+    
+}
